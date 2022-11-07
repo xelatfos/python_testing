b5b2e85f21f5c6ab30647da659d980257f4653f1
xelatfos <kronborn1@gmail.com>
2022-11-07 18:13:10 +0300

    Modify is safe for jenkins now

--- Tests/test_modify_group.py
+++ Tests/test_modify_group.py
@@ -7,7 +7,7 @@ def test_modify_groups(app):
     app.main_page.menu_groups.click()
     app.main_page.chk_grp_first.click()
     app.main_page.btn_edit_group.click()
-    app.main_page.fill_grp(Group(name="name11"))
+    app.main_page.fill_grp(Group("name11","",""))
     app.main_page.btn_grp_update.click()
 
     # MainPage().logout()
