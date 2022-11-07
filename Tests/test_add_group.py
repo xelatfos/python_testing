ac903e07dcc2655e662da740a3ed88bf4d2e99f5
xelatfos <kronborn1@gmail.com>
2022-11-07 19:56:33 +0300

    Empty Group test

--- Tests/test_add_group.py
+++ Tests/test_add_group.py
@@ -6,7 +6,7 @@ from domain.group import Group
 
 def test_add_groups(app):
     with allure.step("Adding a new group"):
-        app.main_page.group_create(Group("name1", "header2", "footer3"))
+        app.main_page.group_create(Group("name1"))
         app.main_page.group_create(Group("name", "header", "footer"))
         app.main_page.group_create(Group("", "", ""))
     # MainPage().logout()
