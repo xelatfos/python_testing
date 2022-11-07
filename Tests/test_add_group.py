d88e826e791e286abf415c780e89a1e180d59181
xelatfos <kronborn1@gmail.com>
2022-11-07 15:39:12 +0300

    Another group add

--- Tests/test_add_group.py
+++ Tests/test_add_group.py
@@ -4,6 +4,7 @@ from domain.group import Group
 from domain.user import User
 
 def test_add_groups(app):
+    app.main_page.group_create(Group("name1", "header2", "footer3"))
     app.main_page.group_create(Group("name", "header", "footer"))
     app.main_page.group_create(Group("", "", ""))
     # MainPage().logout()
