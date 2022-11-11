20f9514cba2f9bac48f2d7fd79a594080e64b364
xelatfos <kronborn1@gmail.com>
2022-11-12 02:22:37 +0300

    Allure step attempt

--- Tests/test_add_group.py
+++ Tests/test_add_group.py
@@ -16,6 +16,7 @@ testdata = [
     for head in ["", rnd_str('head ', 10)]
     for foot in ["", rnd_str('foot ', 10)]
 ]
+@allure.step("Step with two args: {0} and {1}")
 @allure.title("Adding new groups step")
 @pytest.mark.parametrize('group', testdata, ids = [repr(x) for x in testdata])
 def test_add_groups(app, group):
