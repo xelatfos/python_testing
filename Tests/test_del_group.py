c14e331f1721b93d825bfdfd0e380c40f43c32f9
xelatfos <kronborn1@gmail.com>
2022-11-12 02:49:21 +0300

    Allure delete rest groups step attempt

--- Tests/test_del_group.py
+++ Tests/test_del_group.py
@@ -9,13 +9,20 @@ def test_del_group(app):
         app.main_page.group_del(randrange(gr_num))
         assert app.main_page.grp_cnt().chk_grp_cnt == gr_num -1
 
-
-
 def test_del_groups(app):
     with allure.step("Deleting all groups"):
         gr_num = app.main_page.grp_cnt().chk_grp_cnt
         # also we can use enumerate() here
-        for index, chk in zip(range(2), app.main_page.chk_grp_all):
+        for index, chk in zip(range(5), app.main_page.chk_grp_all):
             chk.click()
         app.main_page.btn_del_group.click()
+        assert app.main_page.grp_cnt().chk_grp_cnt == gr_num -5
 
+def test_del_rest_groups(app):
+    with allure.step("Deleting all groups"):
+        gr_num = app.main_page.grp_cnt().chk_grp_cnt
+        # also we can use enumerate() here
+        for chk in app.main_page.chk_grp_all:
+            chk.click()
+        app.main_page.btn_del_group.click()
+        assert app.main_page.grp_cnt().chk_grp_cnt == 0
