71ce924c7ac5c92fe884240654b06304446a8022
xelatfos <kronborn1@gmail.com>
2022-11-12 21:37:27 +0300

    E2E numbering was adjusted 4 for Allure naming

--- Tests/test_del_group.py
+++ Tests/test_del_group.py
@@ -11,8 +11,8 @@ Deleting a random group
 Checking the groups number after deletion
 """)
 def test_del_group(app):
-    allure.dynamic.title(f".{app.next_tid()} Deletion a random group step")
     gr_num = app.main_page.grp_cnt().chk_grp_cnt
+    allure.dynamic.title(f".{app.next_tid()} Deletion a random group step")
     app.main_page.group_del(randrange(gr_num))
     assert app.main_page.grp_cnt().chk_grp_cnt == gr_num - 1
 
@@ -21,8 +21,8 @@ def test_del_group(app):
 def test_del_groups(app):
     gr_num = app.main_page.grp_cnt().chk_grp_cnt
     # also we can use enumerate() here
+    allure.dynamic.title(f".{app.next_tid()} Deletion of {gr_num} groups step")
     for index, chk in zip(range(5), app.main_page.chk_grp_all):
-        allure.dynamic.title(f".{app.next_tid()} Deletion of {index} groups step")
         chk.click()
     app.main_page.btn_del_group.click()
     assert app.main_page.grp_cnt().chk_grp_cnt == gr_num - 5
@@ -30,8 +30,8 @@ def test_del_groups(app):
 @allure.sub_suite("Deleting the groups")
 def test_del_rest_groups(app):
     gr_num = app.main_page.grp_cnt().chk_grp_cnt
+    allure.dynamic.title(f".{app.next_tid()} Deletion of rest {gr_num} group step")
     for index, chk in enumerate(app.main_page.chk_grp_all):
-        allure.dynamic.title(f".{app.next_tid()} Deletion of rest {gr_num - index} group step")
         chk.click()
     app.main_page.btn_del_group.click()
     assert app.main_page.grp_cnt().chk_grp_cnt == 0
